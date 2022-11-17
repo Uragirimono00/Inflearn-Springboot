b0e397c41e466253a2f45a8ec491efa66f081a5f
kairos7578 <gudtj7578@gmail.com>
2022-11-17 17:54:51 +0900

    fisrt commit

---
+++ src/main/java/com/example/inflearnspringboot/InflearnSpringBootApplication.java
@@ -0,0 +1,19 @@
+package com.example.inflearnspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringBootApplication {
+
+    public static void main(String[] args) {
+
+        Hello hello = new Hello();
+        hello.setData("hello");
+        String data = hello.getData();
+        System.out.println("data = " + data);
+
+        SpringApplication.run(InflearnSpringBootApplication.class, args);
+    }
+
+}
